ee835dc7c59762b615f7b38ae4daff9a823f653f
Yannick Heiber <yannick.heiber@mobimeo.com>
2023-06-23 11:33:26 +0200

    Add http logging to debug CI failures

--- modules/jaeger-thrift-http-exporter/src/test/scala/trace4cats/jaeger/JaegerHttpSpanCompleterSpec.scala
+++ modules/jaeger-thrift-http-exporter/src/test/scala/trace4cats/jaeger/JaegerHttpSpanCompleterSpec.scala
@@ -1,10 +1,10 @@
 package trace4cats.jaeger
 
 import java.time.Instant
-
 import cats.effect.IO
 import fs2.Chunk
 import org.http4s.blaze.client.BlazeClientBuilder
+import org.http4s.client.middleware.Logger
 import trace4cats.model.{Batch, CompletedSpan, TraceProcess}
 import trace4cats.test.jaeger.BaseJaegerSpec
 import trace4cats.{CompleterConfig, SemanticTags}
@@ -16,15 +16,18 @@ class JaegerHttpSpanCompleterSpec extends BaseJaegerSpec {
     val updatedSpan =
       span.copy(start = Instant.now(), end = Instant.now(), attributes = span.attributes -- excludedTagKeys)
     val batch = Batch(Chunk(updatedSpan.build(process)))
-    val completer = BlazeClientBuilder[IO].resource.flatMap { client =>
-      JaegerHttpSpanCompleter[IO](
-        client,
-        process,
-        "localhost",
-        14268,
-        config = CompleterConfig(batchTimeout = 50.millis)
-      )
-    }
+    val completer =
+      BlazeClientBuilder[IO].resource
+        .map(Logger.apply(logHeaders = true, logBody = true, logAction = Some(IO.println(_))))
+        .flatMap { client =>
+          JaegerHttpSpanCompleter[IO](
+            client,
+            process,
+            "localhost",
+            14268,
+            config = CompleterConfig(batchTimeout = 50.millis)
+          )
+        }
 
     testCompleter(
       completer,
