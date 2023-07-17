b4978cdbf0b64bff0b0d5f3acb8a35fdeafe3838
Yannick Heiber <yannick.heiber@mobimeo.com>
2023-07-17 13:25:42 +0200

    Try without queueing

--- modules/jaeger-thrift-http-exporter/src/test/scala/trace4cats/jaeger/JaegerHttpSpanCompleterSpec.scala
+++ modules/jaeger-thrift-http-exporter/src/test/scala/trace4cats/jaeger/JaegerHttpSpanCompleterSpec.scala
@@ -1,15 +1,15 @@
 package trace4cats.jaeger
 
-import java.time.Instant
 import cats.effect.IO
+import cats.effect.kernel.Resource
 import fs2.Chunk
 import org.http4s.blaze.client.BlazeClientBuilder
 import org.http4s.client.middleware.Logger
 import trace4cats.model.{Batch, CompletedSpan, TraceProcess}
 import trace4cats.test.jaeger.BaseJaegerSpec
-import trace4cats.{CompleterConfig, SemanticTags, SpanCompleter}
+import trace4cats.{SemanticTags, SpanCompleter}
 
-import scala.concurrent.duration._
+import java.time.Instant
 
 class JaegerHttpSpanCompleterSpec extends BaseJaegerSpec {
   it should "Send a span to jaeger" in forAll { (span: CompletedSpan.Builder, process: TraceProcess) =>
@@ -20,18 +20,14 @@ class JaegerHttpSpanCompleterSpec extends BaseJaegerSpec {
       BlazeClientBuilder[IO].resource
         .map(Logger.apply(logHeaders = true, logBody = false, logAction = Some(IO.println(_))))
         .flatMap { client =>
-          JaegerHttpSpanCompleter[IO](
-            client,
-            process,
-            "localhost",
-            14268,
-            config = CompleterConfig(batchTimeout = 50.millis)
-          ).map { inner =>
-            new SpanCompleter[IO] {
-              override def complete(span: CompletedSpan.Builder): IO[Unit] =
-                inner.complete(span) >> IO.sleep(1.second)
-            }
-          }
+          Resource
+            .eval(JaegerHttpSpanExporter[IO, Chunk](client, process, "localhost", 14268, "http"))
+            .map(exp =>
+              new SpanCompleter[IO] {
+                override def complete(span: CompletedSpan.Builder): IO[Unit] =
+                  exp.exportBatch(Batch(Chunk(span.build(process))))
+              }
+            )
         }
 
     testCompleter(
